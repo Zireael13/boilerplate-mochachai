aa74bab48ee9cf9f859988ff645143b36698047e
Matt Wilkinson <mattwilki17@gmail.com>
2020-09-10 14:54:16 -0400

    e2e link fix

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -171,7 +171,7 @@ suite('Functional Tests', function() {
 
   // On Gomix we'll use this setting
   /** ### Copy your project's url here  ### **/
-  Browser.site = 'https://sincere-cone.gomix.me'; 
+  Browser.site = 'https://fathomless-peak-29481.herokuapp.com/'; 
   
   // If you are testing on a local environment replace the line above  with 
   // Browser.localhost('example.com', (process.env.PORT || 3000));
